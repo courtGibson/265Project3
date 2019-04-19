c9aa7ef22f97a0f74a15b234be59eb2764c7fcde
Courtney Gibson <courtney.gibson@centre.edu>
2019-04-19 15:05:54 -0400

    edit

--- script.js
+++ script.js
@@ -1,6 +1,43 @@
-var dataP = d3.json("runData.json")
+var dataP = d3.json("runningDatajson.json")
 
 dataP.then(function(data)
 {
   console.log(data);
+
+  fifteen = getEventList(data, "1500m")
+  steeple = getEventList(data, "3000m Steeplechase")
+  fivek = getEventList(data, "fivek")
+  tenk = getEventList(data, "tenk")
+  marathon = getEventList(data, "Marathon")
+
+  var events = [fifteen, steeple, fivek, tenk, marathon]
+
+  console.log("events", events)
+
+
 })
+
+var getEventList = function(data, event)
+{
+  var femaleArray = []
+  var maleArray = []
+
+  data.forEach(function(d,i)
+  {
+    if (d.event==event)
+    {
+      if (d.gender == "male")
+      {
+        maleArray.push(d);
+      }
+      else
+      {
+        femaleArray.push(d);
+      }
+    }
+  })
+
+  var event = {female:femaleArray, male:maleArray};
+
+  return event;
+}
